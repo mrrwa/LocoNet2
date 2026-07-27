845c98657b376fe4d2857e40dcad1a0ba1ac5446
Alex Shepherd <kiwi64ajs@gmail.com>
2026-07-27 23:49:53 +1200

    Add PlatformIO compatibility header for Embedded Template Library

---
+++ lib/Embedded_Template_Library.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Compatibility wrapper for PlatformIO builds
+// This header satisfies the `#include <Embedded_Template_Library.h>` directive
+// used in Arduino IDE. The actual ETL functionality comes from <etl/vector.h>
+// and related headers already included separately.
+//
+// This file can be empty because the real ETL headers are included via <etl/vector.h>
